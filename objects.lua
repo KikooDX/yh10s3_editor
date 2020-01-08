c7981d01ec6639611415f9e8b84cafd807898422
KikooDX <30863886+KikooDX@users.noreply.github.com>
2020-01-08 17:03:24 +0100

    Simpler sprite system

--- objects.lua
+++ objects.lua
@@ -1,29 +1,28 @@
 --objects sprites & size
 objects_data = {
   [75] = {
-    sprite = love.graphics.newImage("assets/o/75.png"),
     width  = 16,
     height = 16
   },
   [78] = {
-    sprite = love.graphics.newImage("assets/o/78.png"),
     width  = 16,
     height = 16
   },
   [76] = {
-    sprite = love.graphics.newImage("assets/o/76.png"),
     width  = 16,
     height = 32
   },
   [77] = {
-    sprite = love.graphics.newImage("assets/o/77.png"),
     width  = 16,
     height = 32
   },
   [92] = {
-    sprite = love.graphics.newImage("assets/o/92.png"),
     width  = 16,
     height = 16
   }
 }
 objects_list = { 75, 78, 77, 76, 92 }
+
+for _, v in ipairs(objects_list) do
+  objects_data[v].sprite = love.graphics.newImage("assets/o/"..tostring(v)..".png")
+end
