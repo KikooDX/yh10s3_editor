04312d9fc800eeb1060d375773291eb9299e2db0
KikooDX <kikoodissident@gmail.com>
2020-01-08 10:20:22 +0100

    (WIP) Adding objects in progress

--- mouse.lua
+++ mouse.lua
@@ -1,5 +1,7 @@
 function update_mouse()
   mouse_x, mouse_y = love.mouse.getX(), love.mouse.getY() --mouse position
+  local buffer_x = clip_mouse_x
+  local buffer_y = clip_mouse_y
   clip_mouse_x = mouse_x - mouse_x % grid_spacing
   clip_mouse_y = mouse_y - mouse_y % grid_spacing
   --edition mode
@@ -8,5 +10,9 @@ function update_mouse()
     mouse_mode = 1
   elseif love.mouse.isDown(2) then
     mouse_mode = -1
+  end
+  --add to layer if
+  if mode == 1 and (clip_mouse_x ~= buffer_x or clip_mouse_y ~= buffer_y) then
+    add_to_layer(1)
   end
 end
