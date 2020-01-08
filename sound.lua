f8cd509c0d1036e36655d5f98827dbf7f104f7da
KikooDX <30863886+KikooDX@users.noreply.github.com>
2020-01-08 16:42:05 +0100

    Original sounds from YH10S3 Owned by Tamationgames. Since there is no way to contact him, I couldn't ask for the autorisation to use those assets.

--- sound.lua
+++ sound.lua
@@ -1,3 +1,5 @@
 s_place  = love.audio.newSource("assets/place.wav",  "static")
+s_place:setVolume(0.5)
 s_remove = love.audio.newSource("assets/remove.wav", "static")
+s_place:setVolume(0.5)
 sfx = true
