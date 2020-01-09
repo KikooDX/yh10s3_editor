62a5fa28f087ce84175c2ae81245a952a303e471
KikooDX <kikoodissident@gmail.com>
2020-01-09 14:04:30 +0100

    (WIP) more layer GUI

--- layers_gui.lua
+++ layers_gui.lua
@@ -3,8 +3,11 @@ function draw_gui_layers()
     local pos_i = #layers - i + 1
     if i == layer_selected then love.graphics.setColor(0.8, 0.8, 0.8, 1)
     else love.graphics.setColor(0.2, 0.2, 0.2, 1) end
-    love.graphics.rectangle("line", GAME_WIDTH - 96, pos_i * 18 - 2, 95, 16)
+    rem_y = pos_i * 18 - 2
+    love.graphics.rectangle("line", GAME_WIDTH - 96, rem_y, 95, 16)
     love.graphics.print("Layer "..pos_i, GAME_WIDTH - 92, pos_i * 18 - 1)
+    love.graphics.draw(s_gui.up_down, GAME_WIDTH - 27, rem_y + 2)
+    love.graphics.draw(s_gui.see, GAME_WIDTH - 36, rem_y + 4)
   end
 end
 
--- load.lua
+++ load.lua
@@ -11,16 +11,25 @@ s_cursor = {
   [1]  = love.mouse.newCursor("assets/mouse_cursor_place.png"),
   [-1] = love.mouse.newCursor("assets/mouse_cursor_del.png")
 }
+--gui boutons
+s_gui = {
+  see     = love.graphics.newImage("assets/gui/see.png"),
+  blind   = love.graphics.newImage("assets/gui/blind.png"),
+  delete  = love.graphics.newImage("assets/gui/delete.png"),
+  up_down = love.graphics.newImage("assets/gui/up_down.png"),
+  new     = love.graphics.newImage("assets/gui/new.png")
+}
 --boudaries
 bounds = { min_x = 128, min_y = 16, max_x = 386, max_y = 272 }
 grid_spacing = 8 --object clipping setting
 --layers
 layers = { {}, {}, {} }
+visible_layers = { true, true, true }
 layer_selected = 1
 --mouse_position buffer
 buffer_x, buffer_y = 0, 0
 --shortcuts
-user_shortcuts = {75, 78, 79, 81, 83, 84, 86, 88, 76}
+user_shortcuts = { 75, 78, 79, 81, 83, 84, 86, 88, 76 }
 --object selected
 slot_selected = 1
 object_selected = user_shortcuts[slot_selected]
