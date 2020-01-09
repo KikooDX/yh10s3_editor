ea86cbea19a8ffb1e6c666ec045dd9c2f2a58afe
KikooDX <kikoodissident@gmail.com>
2020-01-09 13:33:21 +0100

    Fully working layer system

--- layers_gui.lua
+++ layers_gui.lua
@@ -1,17 +1,17 @@
 function draw_gui_layers()
   for i = 1, #layers, 1 do
-    pos_i = #layers - i + 1
+    local pos_i = #layers - i + 1
     if i == layer_selected then love.graphics.setColor(0.8, 0.8, 0.8, 1)
     else love.graphics.setColor(0.2, 0.2, 0.2, 1) end
-    love.graphics.rectangle("line", GAME_WIDTH - 96, pos_i * 18 - 2, 96, 16)
-    love.graphics.print("Layer "..i, GAME_WIDTH - 92, pos_i * 18 - 1)
+    love.graphics.rectangle("line", GAME_WIDTH - 96, pos_i * 18 - 2, 95, 16)
+    love.graphics.print("Layer "..pos_i, GAME_WIDTH - 92, pos_i * 18 - 1)
   end
 end
 
 function select_layer()
   for i = 1, #layers, 1 do
     if love.keyboard.isDown("f"..i) then
-      layer_selected = i
+      layer_selected = #layers - i + 1
     end
   end
 end
--- mouse.lua
+++ mouse.lua
@@ -35,15 +35,17 @@ function love.mousepressed(x, y, button)
     local bound_y = 16
     if x >= bound_x and y >= bound_y and
        x <= bound_x + 76 and y <= bound_y + 252 then
-      x = x - bound_x
-      y = y - bound_y
+      local x = x - bound_x
+      local y = y - bound_y
       select_slot(math.floor(x / 26) + 1 + math.floor(y / 82) * 3)
       if sfx then s_scroll:play() end
     end
   end
   --layers
-  if x >= GAME_WIDTH - 96 and y >= GAME_WIDTH and
+  if x >= GAME_WIDTH - 96 and y <= GAME_WIDTH and
      y >= 16 and y <= #layers * 18 + 14 then
-    s_place:play()
-  end 
+    y = math.floor((y - 2) / 16)
+    layer_selected = #layers - y + 1
+    s_scroll:play()
+  end
 end
