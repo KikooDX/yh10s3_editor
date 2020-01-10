376667de77d5edcdd2195fa7b94fdd0888360b69
KikooDX <30863886+KikooDX@users.noreply.github.com>
2020-01-10 18:11:14 +0100

    (s/o) Glue + aligned all that stuff

--- objects.lua
+++ objects.lua
@@ -1,29 +1,31 @@
 --objects sprites & size
 objects_data = {
-  [75] = {}, --bloc
-  [76] = {off_x = 8, off_y = 16}, --exit door
-  [77] = {off_x = 8, off_y = 16}, --entry door
-  [78] = {}, --spike
-  [79] = {}, --conveyor left
-  [80] = {}, --conveyor right
-  [81] = {off_x = 8, off_y = 8}, --teleporter in
-  [82] = {off_x = 8, off_y = 8}, --teleporter out
-  [83] = {}, --key
-  [84] = {}, --button
-  [85] = {}, --button bloc
-  [86] = {}, --gravity arrow up
-  [87] = {}, --gravity arrow down
-  [88] = {}, --water surface
-  [89] = {}, --water
-  [90] = {}, --ice
-  [91] = {}, --laser canon
-  [92] = {}, --invisible bloc
-  [93] = {}, --invisible spike
-  [94] = {}, --fake spike
-  [95] = {}, --invisible ice
-  [96] = {}, --chain
-  [97] = {}, --super conveyor left
-  [98] = {}, --super conveyor right
+  [75]  = {}, --bloc
+  [76]  = {off_x = 8, off_y = 16}, --exit door
+  [77]  = {off_x = 8, off_y = 16}, --entry door
+  [78]  = {}, --spike
+  [79]  = {}, --conveyor left
+  [80]  = {}, --conveyor right
+  [81]  = {off_x = 8, off_y = 8}, --teleporter in
+  [82]  = {off_x = 8, off_y = 8}, --teleporter out
+  [83]  = {}, --key
+  [84]  = {}, --button
+  [85]  = {}, --button bloc
+  [86]  = {}, --gravity arrow up
+  [87]  = {}, --gravity arrow down
+  [88]  = {}, --water surface
+  [89]  = {}, --water
+  [90]  = {}, --ice
+  [91]  = {}, --laser canon
+  [92]  = {}, --invisible bloc
+  [93]  = {}, --invisible spike
+  [94]  = {}, --fake spike
+  [95]  = {}, --invisible ice
+  [96]  = {}, --chain
+  [97]  = {}, --super conveyor left
+  [98]  = {}, --super conveyor right
+  [99]  = {}, --shrink ray
+  [100] = {}, --glue
 }
 
 objects_list = {}
